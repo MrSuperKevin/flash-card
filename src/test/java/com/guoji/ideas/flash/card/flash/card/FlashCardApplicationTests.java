6655e407ad5fbadfa8eb0b45021b9f4217df1334
superhero <firefox1215@126.com>
2022-01-16 20:18:32 +0800

    init

---
+++ src/test/java/com/guoji/ideas/flash/card/flash/card/FlashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoji.ideas.flash.card.flash.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
